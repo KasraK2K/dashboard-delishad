92da0dbab5b4bd59a8d5584162b1b0e1e37a64e1
Kasra <Kasra_K2K@yahoo.com>
2020-10-10 22:24:54 +0330

    fix bug

--- server.js
+++ server.js
@@ -1,37 +1,43 @@
 // express config
-// const express = require("express");
-// const port = process.env.PORT || 8080;
-// const app = express();
-// const path = require("path");
+const express = require("express");
+const port = process.env.PORT || 8080;
+const app = express();
+const path = require("path");
 
-// app.use(express.static("dist"));
+app.use(express.static("dist"));
 
-// app.get(/.*/, (req, res) => {
-//   res.sendFile(path.resolve(__dirname + "/dist/index.html"));
-// });
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/dist/index.html"));
+});
 
-// app.listen(port, () => console.log(`server started at port ${port}`));
+app.listen(port, () => console.log(`server started at port ${port}`));
 
 
 
 // fastify config
-const fastify = require('fastify')({ logger: true });
-const path = require("path");
-const fs = require('fs')
-const port = process.env.PORT || 8080;
+// const fastify = require('fastify')({ logger: true });
+// const fastifyStatic = require('fastify-static');
+// const path = require("path");
+// const fs = require('fs')
+// const port = process.env.PORT || 8080;
 
-fastify.get(/.*/, async (request, reply) => {
-  const stream = fs.createReadStream(path.resolve(__dirname + "/dist/index.html"), 'text/html')
-  reply.send(stream);
-});
+// fastify.register(fastifyStatic, {
+//   root: path.resolve(__dirname + '/dist/'),
+// });
 
-const start = async () => {
-  try {
-    await fastify.listen(port)
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-start()
+// const htmlFile = path.resolve(__dirname + "/dist/index.html");
+
+// fastify.get(/.*/, async (request, reply) => {
+//   reply.sendFile(htmlFile)
+// });
+
+// const start = async () => {
+//   try {
+//     await fastify.listen(port)
+//     fastify.log.info(`server listening on ${fastify.server.address().port}`)
+//   } catch (err) {
+//     fastify.log.error(err)
+//     process.exit(1)
+//   }
+// }
+// start()
