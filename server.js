d9086dd42c63e2b4aaa99b3fdafd7e1324d403e4
Kasra Karami <Kasra_K2K@yahoo.com>
2020-10-10 17:47:54 +0330

    create web server with fastify

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
-const express = require("express");
-const port = process.env.PORT || 8080;
-const app = express();
+// express config
+// const express = require("express");
+// const port = process.env.PORT || 8080;
+// const app = express();
+// const path = require("path");
+
+// app.use(express.static("dist"));
+
+// app.get(/.*/, (req, res) => {
+//   res.sendFile(path.resolve(__dirname + "/dist/index.html"));
+// });
+
+// app.listen(port, () => console.log(`server started at port ${port}`));
+
+
+
+// fastify config
+const fastify = require('fastify')({ logger: true });
 const path = require("path");
+const fs = require('fs')
+const port = process.env.PORT || 8080;
 
-app.use(express.static("dist"));
-
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/dist/index.html"));
+fastify.get(/.*/, async (request, reply) => {
+  const stream = fs.createReadStream(path.resolve(__dirname + "/dist/index.html"), 'text/html')
+  reply.send(stream);
 });
 
-app.listen(port, () => console.log(`server started at port ${port}`));
+const start = async () => {
+  try {
+    await fastify.listen(port)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
